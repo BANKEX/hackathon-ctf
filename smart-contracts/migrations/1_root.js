657a23a65c9c806f23340e98a77669c16129a6c4
Kirill <kk@bankexfoundation.org>
2018-07-02 20:46:24 +0300

    fix

--- smart-contracts/migrations/1_root.js
+++ smart-contracts/migrations/1_root.js
@@ -1,10 +1,10 @@
 var Root = artifacts.require("./Root.sol");
 
-var tasks = ["FastFlow", "JoiCasino", "LEGO", "Kamikaze", "Vault", "FactoryDoubleTapVote"];
+var tasks = ["FastFlow", "JoiCasino", "LEGO", "Kamikaze", "Vault"];
 var cryptoAddresses = ["0x635a457b2c04cfc57a6d6553b2254af624c42e23", "0x98af0a02278fa978d0cdd006f0dab3692cd0d680", "0xdeeb17af769dd158fccb12208907833f2dd03215", "0xd52f3792c3afabd246113ef058c5671b6baa0191"];
 
 
-
+// Before "truffle migrate" - comment DoubleTapVote and it factory because maybe assembler dont read
 module.exports = function(deployer, network, accounts) {
   async function deploy(contract, operator, params){
     params = typeof params !== 'undefined' ?  params : [];
