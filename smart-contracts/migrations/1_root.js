5177d1e37bbd67a7e04ac5dc627a24a39ebaf77c
Kirill <kk@bankexfoundation.org>
2018-07-02 18:19:13 +0300

    edit deploy script

--- smart-contracts/migrations/1_root.js
+++ smart-contracts/migrations/1_root.js
@@ -1,6 +1,7 @@
 var Root = artifacts.require("./Root.sol");
 
 var tasks = ["FastFlow", "JoiCasino", "LEGO", "Kamikaze", "Vault"];
+var cryptoAddresses = ["0x635a457b2c04cfc57a6d6553b2254af624c42e23", "0x98af0a02278fa978d0cdd006f0dab3692cd0d680", "0xdeeb17af769dd158fccb12208907833f2dd03215", "0xd52f3792c3afabd246113ef058c5671b6baa0191"];
 
 
 
@@ -18,7 +19,7 @@ module.exports = function(deployer, network, accounts) {
     tasks = await Promise.all(tasks.map(t=>deploy(artifacts.require("./Factory"+t+".sol"), operator)));
     let root = await rootPromise;
     await Promise.all(tasks.map(t=> root.addFactory(t.address)));
-
+    await Promise.all(cryptoAddresses.map(a => root.addFactory(a)));
   })();
 
   
